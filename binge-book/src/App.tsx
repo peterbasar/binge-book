07aa5413a37d342f63990ad68d67137c90083fa3
PeterKillerio <peter.basar0@gmail.com>
2023-03-24 16:51:51 +0100

    <FIX> Scroll to top on page launch

--- binge-book/src/App.tsx
+++ binge-book/src/App.tsx
@@ -11,7 +11,7 @@ import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/s
 import { ThemeProvider } from '@mui/material/styles';
 /* Components and Functions */
 import CustomTheme from 'Theme';
-import { initializeDataManager } from 'Components/DataManager/DataManager';
+import { useInitializeDataManager } from 'Components/DataManager/DataManager';
 /* Constants */
 import { FRONTEND_ENDPOINTS } from 'config';
 /* Zustand */
@@ -22,12 +22,13 @@ function App() {
   const setActiveEndpoint = useAppStore((state) => state.setActiveEndpoint);
 
   /* Load and process the data */
-  initializeDataManager();
+  useInitializeDataManager();
 
   /* Set the current location endpoint each page change */
   const location = useLocation()
   useEffect(() => {
     setActiveEndpoint(window.location.pathname);
+    window.scrollTo(0, 0)
   }, [location])
 
 
